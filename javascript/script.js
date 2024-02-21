2e0f0aeca5a0d94606a4c3c1387d30a914e368e8
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2024-02-20 21:53:39 -0300

    Aula 1

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nome = '';
+        this.valor = 0;
+
+    }
+    adicionar(){
+        alert('Item adicionado!');
+    }
+
+    excluir(){
+        alert('Item deletado!');
+    }
+
+    atualizar(){
+
+    }
+
+    ler(){
+
+    }
+
+}
+
+var produto = new Produto(); 
