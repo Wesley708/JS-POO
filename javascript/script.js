5df13e92d1e98d90c20fac66d9b7cbac7edef89a
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2024-02-25 15:32:03 -0300

    Alteração que não lembro bem oq foi nem se ta certo

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,52 @@
-class Produto{
+var container = document.querySelector(".container");
+
+var campoNome = document.querySelector("#nome");
+var campoIdade = document.querySelector("#idade");
+
+class Pessoa{
 
     constructor(){
-        this.id = 0;
-        this.nome = '';
-        this.valor = 0;
-
+        this.valor = [];
     }
-    adicionar(){
-        alert('Item adicionado!');
+    create(pessoa){
+        this.valor.push(pessoa);
     }
 
-    excluir(){
-        alert('Item deletado!');
+    read(){
+        container.innerHTML = ''; 
+        function reader(element){
+            container.innerHTML += `<div class="box">
+                <p class="item">Id: ${element.id}</p> 
+                <p class="item">Nome: ${element.nome} </p> 
+                <p class="item"> Idade: ${element.idade}</p>
+                <button class="deletar" onclick="deletar(${element.id})">Deletar</button>
+            </div> `;
+        }
+        console.log(this.valor);
+        this.valor.forEach(reader);
     }
 
-    atualizar(){
+    update(id){
 
     }
 
-    ler(){
-
+    delete(id){
+        this.valor.splice(id, 1);
+        pessoa.read();
     }
 
 }
 
-var produto = new Produto(); 
+var pessoa = new Pessoa(); 
+
+var id = 0;
+function create(){
+    pessoa.create({id: id++, nome: campoNome.value, idade: campoIdade.value});
+    pessoa.read();
+}
+
+function deletar(id){
+    pessoa.delete(id);
+};
+
+pessoa.read()
